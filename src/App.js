0acdde99c0119f6e9efb286b73d3261b6fdaefe1
Katarina Farkas <115600624+Katarina-411@users.noreply.github.com>
2023-04-07 16:43:28 +0930

    Initial set-up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from "react"
+import Search from "./Search"
+
+export default function App() {
+  return (
+    <div className="App">
+      <Search />
+    </div>
+  );
+}
+
---
+++ src/Search.js
@@ -0,0 +1,4 @@
+import React from "react";
+ export default function Search() {
+    return (<div><h1>Dictionary</h1> <p> What word do you want to look up?</p></div>)
+ }
