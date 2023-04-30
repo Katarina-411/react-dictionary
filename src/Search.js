74bc86726fe33d2e2385503eafe93673aac4c70c
Katarina Farkas <411digitaldesign@gmail.com>
2023-04-30 13:27:36 +0930

    Rendered search results and meanings

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,14 @@
 import React, {useState} from "react";
+import Results from "./Results";
 import axios from "axios";
 import "./Search.css";
 
 export default function Search() {
 let [keyword, setKeyword]=useState(null);
+let [results, setResults]=useState(null);
 
 function handleResponse(response){
-   console.log(response.data[0]);
+   setResults(response.data[0]);
 }
 
 function handleSearch(event) {
@@ -25,5 +27,6 @@ function handleKeywordChange(event) {
          <input type="search" placeholder="Search for a word" onChange={handleKeywordChange}/>
          <input type="submit" value="Search" id="search-button"/>
       </form>
+      <Results results={results}/>
     </div>);
  }
