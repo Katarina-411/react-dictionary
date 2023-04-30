d87c5e1101d15f3833b76a1fb8d0e590eef04bcb
Katarina Farkas <411digitaldesign@gmail.com>
2023-04-30 12:09:55 +0930

    Add Search engine, header & footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,33 @@ import Search from "./Search"
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1> 
+          <p> What word do you want to look up?</p>
+          </header>
       <Search />
+      <footer className="App-footer"><p>
+          This application was coded by{" "}
+          <a
+            href="https://katarina-farkas.netlify.app/"
+            target="blank"
+            rel="no-referrer"
+          >
+            Katarina Farkas
+          </a>{" "}
+          using ReactJS{" "}
+          <img src="./logo192.png" alt="React logo" class="App-logo" /> and is{" "}
+          <a
+            href="https://github.com/Katarina-411/react-dictionary.git"
+            target="blank"
+            rel="no-referrer"
+          >
+            open-sourced
+          </a>{" "}
+          on GitHub
+        </p></footer>
+      </div>
     </div>
   );
 }
--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,23 @@
-import React from "react";
- export default function Search() {
-    return (<div><h1>Dictionary</h1> <p> What word do you want to look up?</p></div>)
+import React, {useState} from "react";
+import "./Search.css";
+
+export default function Search() {
+let [keyword, setKeyword]=useState(null);
+
+function handleSearch(event) {
+   event.preventDefault();
+   alert(`Searching definition for ${keyword}`);
+}
+function handleKeywordChange(event) {
+   setKeyword(event.target.value);
+
+}
+
+    return (
+    <div>
+      <form onSubmit={handleSearch}>
+         <input type="search" placeholder="Search for a word" onChange={handleKeywordChange}/>
+         <input type="submit" value="Search" id="search-button"/>
+      </form>
+    </div>);
  }
