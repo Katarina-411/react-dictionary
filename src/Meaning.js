f163d4a236ba6a1d6e8b7da61687dd13f76f78b6
Katarina Farkas <411digitaldesign@gmail.com>
2023-04-30 14:24:58 +0930

    Rendered synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
     return (
@@ -9,7 +10,8 @@ export default function Meaning(props) {
             <div key={index}>
                 <p> {definition.definition}
                 <br />
-                <em>{definition.example}</em></p></div>);
+                <em>{definition.example}</em></p>
+                <Synonyms synonyms={definition.synonyms} /></div>);
             })}
                </div>);
 }
