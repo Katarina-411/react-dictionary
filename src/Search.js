28635206798a186b1ac1e2ce198291e5d76f2b23
Katarina Farkas <411digitaldesign@gmail.com>
2023-04-30 12:34:03 +0930

    Added dictionary API integration

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,18 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Search.css";
 
 export default function Search() {
 let [keyword, setKeyword]=useState(null);
 
+function handleResponse(response){
+   console.log(response.data[0]);
+}
+
 function handleSearch(event) {
    event.preventDefault();
-   alert(`Searching definition for ${keyword}`);
+   let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+   axios.get(apiUrl).then(handleResponse);
 }
 function handleKeywordChange(event) {
    setKeyword(event.target.value);
