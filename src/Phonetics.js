922c45d3a17a21cbcb46d226f674df67cf35c496
Katarina Farkas <411digitaldesign@gmail.com>
2023-05-14 13:50:54 +0930

    Add rel="noreferrer" to Phonetics audio link

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,7 +2,7 @@ import React from "react";
 export default function Phonetics (props){
     console.log(props.phonetics.text);
     return(<span className="phonetics">
-        <a href={props.phonetics.audio} target="_blank">
+        <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
         <i class="fa-solid fa-volume-high"></i> 
         </a>
         {" "}
