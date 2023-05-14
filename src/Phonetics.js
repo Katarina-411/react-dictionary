c6b05a6a43e4a337e9ff4e7df7874eb19a839fc5
Katarina Farkas <411digitaldesign@gmail.com>
2023-05-14 13:48:34 +0930

    Added Phonetics component to Results

---
+++ src/Phonetics.js
@@ -0,0 +1,11 @@
+import React from "react";
+export default function Phonetics (props){
+    console.log(props.phonetics.text);
+    return(<span className="phonetics">
+        <a href={props.phonetics.audio} target="_blank">
+        <i class="fa-solid fa-volume-high"></i> 
+        </a>
+        {" "}
+        {props.phonetics.text}
+    </span>);
+}
--- src/Results.js
+++ src/Results.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
     if (props.results) {
     return <div className="results">
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function(phonetics, index){
+            return (
+                <div key={index}>
+                <Phonetics phonetics={phonetics}/>
+                </div>);
+        })}
         {props.results.meanings.map(function(meaning, index) {
 return (<div key={index}>
     <Meaning meaning={meaning}/>
