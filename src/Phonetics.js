65d17ba91dc67228d3d961e9c2126424c84a1e10
Katarina Farkas <411digitaldesign@gmail.com>
2023-05-14 19:51:31 +0930

    Added image API and final styling

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Phonetics (props){
-    console.log(props.phonetics.text);
     return(<span className="phonetics">
         <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
         <i class="fa-solid fa-volume-high"></i> 
--- src/Search.js
+++ src/Search.js
@@ -1,20 +1,34 @@
 import React, {useState} from "react";
 import Results from "./Results";
+import Photos from "./Photos";
 import axios from "axios";
 import "./Search.css";
 
 export default function Search() {
 let [keyword, setKeyword]=useState(null);
 let [results, setResults]=useState(null);
+let [photos, setPhotos]=useState(null);
 
-function handleResponse(response){
+function handleSearchResponse(response){
    setResults(response.data[0]);
+}
+
+function handlePexelsResponse(response){
+setPhotos(response.data.photos);
+}
+
+function searchKeyword() {
+   let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+   axios.get(apiUrl).then(handleSearchResponse);
+
+   let imageApiKey="5979b3odca904f12bbfe10t39f04351c";
+   let imageApiUrl=`https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imageApiKey}`;
+   axios.get(imageApiUrl).then(handlePexelsResponse);
 }
 
 function handleSearch(event) {
    event.preventDefault();
-   let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-   axios.get(apiUrl).then(handleResponse);
+   searchKeyword();
 }
 function handleKeywordChange(event) {
    setKeyword(event.target.value);
@@ -28,7 +42,9 @@ function handleKeywordChange(event) {
          <input type="search" placeholder="Search for a word" onChange={handleKeywordChange} id="search-box"/>
          <input type="submit" value="Search" id="search-button"/>
       </form>
+      <p>Results include phonetic info, definitions, examples, synonyms and images (where applicable).</p>
       </section>
       <Results results={results}/>
+      <Photos photos={photos}/>
     </div>);
  }
